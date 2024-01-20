7cbf7a290748873ca95b18baf50ac49645f4ef7a
Taj Pereira <tajpereira@gmail.com>
2024-01-21 08:51:23 +1030

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { vanillaImpl } from "./vanilla_impl";
 import { SQUARES_TO_RENDER } from "./constants";
