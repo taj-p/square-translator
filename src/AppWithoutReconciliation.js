b8d45181ebf4ed180b0454de85061dfdceb99b89
Taj Pereira <tajpereira@gmail.com>
2024-01-21 11:35:20 +1030

    fix: show w/ and w/o reconciliation

---
+++ src/AppWithoutReconciliation.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { SQUARES_TO_RENDER } from "./constants";
+import Square from "./Square";
+
+const App = () => {
+  const [squares, setSquares] = useState([]);
+  // Generate squares on initial render
+  React.useEffect(() => {
+    const newSquares = [];
+    for (let i = 0; i < SQUARES_TO_RENDER; i++) {
+      newSquares.push({
+        id: i,
+        left: Math.random() * window.innerWidth,
+        top: Math.random() * window.innerHeight,
+      });
+    }
+    setSquares(newSquares);
+  }, []);
+
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    const node = ref.current;
+    if (!node) {
+      return;
+    }
+
+    let startPos = { x: 0, y: 0 };
+    let dragPos = { x: 0, y: 0 };
+    let isDragging = false;
+
+    const handleMouseDown = (e) => {
+      isDragging = true;
+      startPos = {
+        x: e.clientX - dragPos.x,
+        y: e.clientY - dragPos.y,
+      };
+    };
+
+    const handleMouseMove = (e) => {
+      if (!isDragging) {
+        return;
+      }
+      dragPos = {
+        x: e.clientX - startPos.x,
+        y: e.clientY - startPos.y,
+      };
+      node.style.transform = `translate(${dragPos.x}px, ${dragPos.y}px)`;
+    };
+
+    const handleMouseUp = () => {
+      isDragging = false;
+    };
+    node.addEventListener("mousedown", handleMouseDown);
+    node.addEventListener("mousemove", handleMouseMove);
+    node.addEventListener("mouseup", handleMouseUp);
+  }, [ref.current]);
+
+  return (
+    <div id="container" ref={ref}>
+      {squares.map((square) => (
+        <Square key={square.id} left={square.left} top={square.top} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AppWithoutReconciliation from "./AppWithoutReconciliation";
 import { vanillaImpl } from "./vanilla_impl";
 import { SQUARES_TO_RENDER } from "./constants";
 
 const queryParams = new URLSearchParams(window.location.search);
 const useReact = queryParams.get("react") === "true";
+const useReconciliation = queryParams.get("reconciliation") === "true";
 
 const startTime = performance.now();
 if (useReact) {
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+  if (useReconciliation) {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  } else {
+    root.render(
+      <React.StrictMode>
+        <AppWithoutReconciliation />
+      </React.StrictMode>,
+      document.getElementById("root"),
+    );
+  }
 } else {
   vanillaImpl();
 }
@@ -23,11 +34,20 @@ if (useReact) {
 
 const squaresRenderedElement = document.getElementById("squaresRendered");
 squaresRenderedElement.textContent = `Squares Rendered: ${SQUARES_TO_RENDER}`;
+
+const typeElement = document.getElementById("type");
+typeElement.textContent = `Type: ${
+  useReact
+    ? useReconciliation
+      ? "React w/ Reconciliation"
+      : "React skip reconciliation"
+    : "Vanilla"
+}`;
+
 const renderTimeElement = document.getElementById("renderTime");
 const fpsElement = document.getElementById("fps");
 
 let lastFrameTime = Date.now();
-let frameRequest = 0;
 let averageFPS = 0;
 const decay = 0.9; // You can adjust this value for more or less smoothing
 
@@ -46,7 +66,7 @@ function updateFPS() {
   }
 
   fpsElement.textContent = `FPS: ${Math.round(averageFPS)} `;
-  frameRequest = requestAnimationFrame(updateFPS);
+  requestAnimationFrame(updateFPS);
 }
 
 let interval = setInterval(() => {
